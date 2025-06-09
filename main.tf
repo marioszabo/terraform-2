24e0190c90caf3c5ac8f4e013cc80abee80178ff
Mario <mareetor@gmail.com>
2025-06-09 16:02:41 +0200

    added a virtual network

--- main.tf
+++ main.tf
@@ -23,3 +23,10 @@ resource "azurerm_resource_group" "MyResource" {
   name     = "example-name"
   location = "eastus"
 }
+
+resource "azurerm_virtual_network" "MyNetwork" {
+  name                = "my-network"
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.MyResource.name
+  address_space       = ["10.0.0.0/16"]
+}
